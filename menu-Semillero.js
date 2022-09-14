cf9182c3cb34afb83033a5976a1e32dc2834099c
dan984git <daniel.984@hotmail.com>
2022-09-14 12:24:02 -0500

    Agregar etiqueta ejemplo

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -4,12 +4,8 @@
 			"MNU_DBP": "Daniel Burbano",
 			"MNU_SAR": "Sebastian Arellano", 
 			"MNU_ENI": "Estefania Nicolalde",
-<<<<<<< HEAD
-			"MNU_TDLC": "Tatiana De La Cruz"
-			
-=======
+			"MNU_TDLC": "Tatiana De La Cruz",
 			"MNU_EJM": "Ejemplo"
->>>>>>> cdf18522b5aa9f9e997a5da061a825e771498004
 		}
     }
 }
