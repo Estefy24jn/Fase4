ee84b804e6ce3b9706afae41336a424e71406b9f
Sebas_jac <sebas_jac@hotmail.es>
2022-09-14 12:37:17 -0500

    Agrega todos menus SAR

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -2,8 +2,12 @@
     "COMMONS": {
 		"MENU" : {
 			"MNU_DBP": "Daniel Burbano",
-			"MNU_SAR": "Sebastian Arellano"
-			
+			"MNU_SAR": "Sebastian Arellano",
+			"MNU_SAR_BVDA": "Bienvenida",
+			"MNU_SAR_CP":"Calcular Promedio",
+			"MNU_SAR_GC":"Guardar Cliente",
+			"MNU_SAR_CC":"Consultar Cliente",
+			"MNU_SAR_CCS":"Consultar Clientes"
 		}
     }
 }
