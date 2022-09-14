5aecdba5b2debd667cf4e28406fa2b0570b0a9f0
dan984git <daniel.984@hotmail.com>
2022-09-14 11:56:26 -0500

    Añadir Etiqueta de Ejemplo

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -3,7 +3,8 @@
 		"MENU" : {
 			"MNU_DBP": "Daniel Burbano",
 			"MNU_SAR": "Sebastian Arellano", 
-			"MNU_ENI": "Estefania Nicolalde"
+			"MNU_ENI": "Estefania Nicolalde",
+			"MNU_EJM": "Ejemplo"
 			
 		}
     }
