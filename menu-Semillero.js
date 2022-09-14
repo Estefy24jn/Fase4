5adee9f9427a7a353d03bc3aee1e4458284ff754
Estefy24jn <105861816+Estefy24jn@users.noreply.github.com>
2022-09-14 11:46:39 -0500

    menu estefania

--- menu-Semillero.js
+++ menu-Semillero.js
@@ -2,7 +2,8 @@
     "COMMONS": {
 		"MENU" : {
 			"MNU_DBP": "Daniel Burbano",
-			"MNU_SAR": "Sebastian Arellano"
+			"MNU_SAR": "Sebastian Arellano", 
+			"MNU_ENI": "Estefania Nicolalde"
 			
 		}
     }
